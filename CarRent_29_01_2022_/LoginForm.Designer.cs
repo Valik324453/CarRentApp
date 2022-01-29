3bbf1dded9ae4fff96a2c3fcef5713fcfe42e270
valik <valik324453@mail.ru>
2022-01-29 10:38:55 +0200

    Username and password inputs. Login button

--- CarRent_29_01_2022_/LoginForm.Designer.cs
+++ CarRent_29_01_2022_/LoginForm.Designer.cs
@@ -30,35 +30,93 @@ namespace CarRent_29_01_2022_
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lUsername = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lPassword = new System.Windows.Forms.Label();
+            this.bLogin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::CarRent_29_01_2022_.Properties.Resources._2048px_User_icon_2_svg;
-            this.pictureBox1.Location = new System.Drawing.Point(310, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(34, 32);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(171, 168);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // lUsername
+            // 
+            this.lUsername.AutoSize = true;
+            this.lUsername.Location = new System.Drawing.Point(236, 32);
+            this.lUsername.Name = "lUsername";
+            this.lUsername.Size = new System.Drawing.Size(73, 17);
+            this.lUsername.TabIndex = 1;
+            this.lUsername.Text = "Username";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(337, 32);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(200, 22);
+            this.tbUsername.TabIndex = 2;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(337, 60);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(200, 22);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // lPassword
+            // 
+            this.lPassword.AutoSize = true;
+            this.lPassword.Location = new System.Drawing.Point(236, 60);
+            this.lPassword.Name = "lPassword";
+            this.lPassword.Size = new System.Drawing.Size(69, 17);
+            this.lPassword.TabIndex = 3;
+            this.lPassword.Text = "Password";
+            // 
+            // bLogin
+            // 
+            this.bLogin.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.bLogin.Location = new System.Drawing.Point(326, 176);
+            this.bLogin.Name = "bLogin";
+            this.bLogin.Size = new System.Drawing.Size(75, 23);
+            this.bLogin.TabIndex = 5;
+            this.bLogin.Text = "Login";
+            this.bLogin.UseVisualStyleBackColor = false;
+            // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(575, 450);
+            this.Controls.Add(this.bLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lUsername);
             this.Controls.Add(this.pictureBox1);
             this.Name = "LoginForm";
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lUsername;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lPassword;
+        private System.Windows.Forms.Button bLogin;
     }
 }
 
